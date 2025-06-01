164e68ac26e8afc97ad5381152fcba5ef50e8eb1
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-06-01 18:31:48 +0200

    Add modularity and handle errors in main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,5 @@
-use std::{env, fs, process};
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &str> {
-        if (args.len()) < 3 {
-            return Err("Not enough arguments.");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -28,6 +12,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
