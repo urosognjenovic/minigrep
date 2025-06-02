94d0af63f4bdfe699b216cf1eac0467c75699aa8
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-06-02 22:27:24 +0200

    Print error messages to standard error and remove poem.txt

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {err}");
+        eprintln!("Error parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
